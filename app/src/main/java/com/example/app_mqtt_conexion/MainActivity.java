dddb9b6ae1df75d3e360f9ec8c0d02d044d479d8
KevinSebastian2023 <116039224+KevinSebastian2023@users.noreply.github.com>
2022-11-19 22:01:23 -0300

    down

--- app/src/main/java/com/example/app_mqtt_conexion/MainActivity.java
+++ app/src/main/java/com/example/app_mqtt_conexion/MainActivity.java
@@ -3,140 +3,18 @@ package com.example.app_mqtt_conexion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import org.eclipse.paho.android.service.MqttAndroidClient;
-import org.eclipse.paho.client.mqttv3.IMqttActionListener;
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.IMqttToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends AppCompatActivity {
 
-    MqttAndroidClient client;
-    TextView subText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        subText = (TextView)findViewById(R.id.subText);
 
-        String clientId = MqttClient.generateClientId();
-        client = new MqttAndroidClient(this.getApplicationContext(), "tcp://68.183.119.177",clientId);
-        //client = new MqttAndroidClient(this.getApplicationContext(), "tcp://192.168.43.41:1883",clientId);
 
-        try {
-            IMqttToken token = client.connect();
-            token.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    Toast.makeText(MainActivity.this,"Conectado ",Toast.LENGTH_LONG).show();
-                    setSubscription();
-
-                }
-
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    Toast.makeText(MainActivity.this,"CONEXION FALLIDA!!",Toast.LENGTH_LONG).show();
-                }
-            });
-        } catch (MqttException e) {
-            e.printStackTrace();
         }
-
-        client.setCallback(new MqttCallback() {
-            @Override
-            public void connectionLost(Throwable cause) {
-
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                subText.setText(new String(message.getPayload()));
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) {
-
-            }
-        });
-
-    }
-
-    public void published(View v){
-
-        String topic = "st";
-        String message = "test";
-        try {
-            client.publish(topic, message.getBytes(),0,false);
-            Toast.makeText(this,"Mensaje publicado con el topico ST ",Toast.LENGTH_SHORT).show();
-        } catch ( MqttException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void setSubscription(){
-
-        try{
-
-            client.subscribe("st",0);
-
-
-        }catch (MqttException e){
-            e.printStackTrace();
-        }
-    }
-
-    public void conn(View v){
-
-        try {
-            IMqttToken token = client.connect();
-            token.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    Toast.makeText(MainActivity.this,"CONECTADO AL SERVIDOR MQTT",Toast.LENGTH_LONG).show();
-                    setSubscription();
-
-                }
-
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    Toast.makeText(MainActivity.this,"CONEXION FALLIDA!!",Toast.LENGTH_LONG).show();
-                }
-            });
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public void disconn(View v){
-
-        try {
-            IMqttToken token = client.disconnect();
-            token.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    Toast.makeText(MainActivity.this,"DESCONECTADO DEL SERVIDOR MQTT",Toast.LENGTH_LONG).show();
-
-
-                }
-
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    Toast.makeText(MainActivity.this,"NO DESCONECTADO!",Toast.LENGTH_LONG).show();
-                }
-            });
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-    }
 
 }
