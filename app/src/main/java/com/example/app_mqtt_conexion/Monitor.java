17cb4f1e65b6b25770008f9544f4ae5ab5075c0c
KevinSebastian2023 <116039224+KevinSebastian2023@users.noreply.github.com>
2022-11-26 20:45:19 -0300

    Configurando notificaciones version 1.0

--- app/src/main/java/com/example/app_mqtt_conexion/Monitor.java
+++ app/src/main/java/com/example/app_mqtt_conexion/Monitor.java
@@ -1,11 +1,24 @@
 package com.example.app_mqtt_conexion;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.graphics.Color;
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -26,10 +39,20 @@ public class Monitor extends AppCompatActivity {
     TextView oxigeno;
     TextView pulsaciones;
 
+    //variables a utilizar para las notificaciones
+
+    private static final String CHANNEL_ID = "NOTIFICACION";
+    public static final int NOTIFICACION_ID = 0;
+    PendingIntent pendingIntent;
+    PendingIntent siPending, noPending;
+
+    private static final int PENDING_REQUEST = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
+
 
 
         oxigeno = (TextView) findViewById(R.id.texto_oxigeno);
@@ -136,6 +159,22 @@ public class Monitor extends AppCompatActivity {
 
 
                 }
+                if (Objects.equals(topic, "st/n")){     //topico y subtopico de la notificacion
+
+
+                    String Message = message.toString();
+                    if(Message.equals("1")){
+
+                        setNoPendingIntent();
+                        setSiPendingIntent();
+                        setPendingIntent();
+                        createNotificacionChannel();
+                        crearNotificaciones();
+                    }
+
+
+
+                }
             }
 
             @Override
@@ -150,13 +189,81 @@ public class Monitor extends AppCompatActivity {
 
         try {
 
-            client.subscribe("st/ir", 0);
-            client.subscribe("st/r", 0);
+            client.subscribe("st/ir", 0); //sensor de nivel de oxigeno
+            client.subscribe("st/r", 0);  //sensor de pulsaciones por minuto
+            client.subscribe("st/n", 0);  // alerta
 
 
         } catch (MqttException e) {
             e.printStackTrace();
         }
     }
+    private void createNotificacionChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            //Características del Canal
+            CharSequence name="Notificacion Normal";
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,name,NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            //notificationChannel.setAllowBubbles(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+    //Inferiores a Oreo API 26 Android 8.0
+    private void crearNotificaciones(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.corazon);
+        builder.setContentTitle("Nombre Notificacion");
+        builder.setContentText("Texto Notificación");
+        builder.setStyle(new NotificationCompat.BigTextStyle()
+                .bigText("Texto largo para que no cabe en una única línea"));
+        builder.setColor(Color.BLUE);
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setLights(Color.MAGENTA,1000,1000);
+        builder.setVibrate(new long[]{1000,1000,1000, 1000});
+        //builder.setSound()
+        builder.setDefaults(Notification.DEFAULT_SOUND);
+        builder.setContentIntent(pendingIntent);
+        builder.setNumber(7);
+        builder.addAction(R.drawable.ic_launcher_background,"Sí", siPending);
+        builder.addAction(R.drawable.ic_launcher_background,"No", noPending);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+        notificationManagerCompat.notify(NOTIFICACION_ID,builder.build());
+    }
 
+    private void setNoPendingIntent() {
+        Intent intent = new Intent(this, Ubicacion_maps.class);
+
+        //Para que al dar hacia atrás vaya a la main y no salga (opcional)
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(Ubicacion_maps.class);
+        stackBuilder.addNextIntent(intent);
+
+
+        noPending = stackBuilder.getPendingIntent(PENDING_REQUEST, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private void setSiPendingIntent() {
+        Intent intent = new Intent(this, Ubicacion_maps.class);
+
+        //Para que al dar hacia atrás vaya a la main y no salga (opcional)
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(Ubicacion_maps.class);
+        stackBuilder.addNextIntent(intent);
+
+
+        siPending = stackBuilder.getPendingIntent(PENDING_REQUEST, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+    private void setPendingIntent() {
+        Intent intent = new Intent(this, Ubicacion_maps.class);
+
+        //Para que al dar hacia atrás vaya a la main y no salga (opcional)
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(Ubicacion_maps.class);
+        stackBuilder.addNextIntent(intent);
+
+
+        pendingIntent = stackBuilder.getPendingIntent(PENDING_REQUEST, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+    }
 }
